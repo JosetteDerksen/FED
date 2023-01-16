98ff419fbf4159f6108d42d5b7bd974c5e60bb19
JosetteDerksen <70659177+JosetteDerksen@users.noreply.github.com>
2023-01-16 11:42:21 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
-console.log("hi");
+
+var menuButton = document.querySelector("header > button");
+console.log(menuButton);
+var deNav = document.querySelector("header nav");
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  deNav.classList.toggle("open");
+}
+
+
+menuButton.addEventListener("click", kruis);
+
+function kruis() {
+  menuButton.classList.toggle("clicked");
+}

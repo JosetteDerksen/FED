385f13cea3f22cb87e0995cc8af0e0d24d6bc144
JosetteDerksen <70659177+JosetteDerksen@users.noreply.github.com>
2023-01-24 18:58:14 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -21,24 +21,16 @@ function kruis() {
 
 // Footer menu
 var footerButtons = document.querySelectorAll("footer nav h4");
-// var eersteFooterSection = document.querySelector("footer nav section:nth-of-type(1)");
-
-// eersteFooterButton.addEventListener("click", toggleEersteFooter);
 
 footerButtons.forEach(footerButton => {
   footerButton.addEventListener("click", toggleFooter);
 });
 
-
-// function toggleEersteFooter() {
-//   eersteFooterSection.classList.toggle("open");
-// }
-
 function toggleFooter(event){
-  // event.target.closest("section").classList.toggle("open");
   event.target.nextElementSibling.classList.toggle("open");
   event.target.classList.toggle("open");
 }
+
 
 
 // Hamburgermenu knop zeggen open en dicht
@@ -54,3 +46,5 @@ hamburgerButton.addEventListener('click', function() {
         menuOpen = true;
     }
 });
+
+
